b6df309092729a05a8ca1fcb7673b0bf19ffd688
hsyngkc <115035858+hsyngkc@users.noreply.github.com>
2022-10-05 14:22:50 +0300

    göremedim

---
+++ src/test/java/day03/Karsılastırma.java
@@ -0,0 +1,17 @@
+package day03;
+
+public class Karsılastırma {
+    public static void main(String[] args) {
+        int A=10;
+        int B =20;
+        int C=10;
+        int D=40;
+
+        System.out.println(A==B);
+        System.out.println(A==C);
+        System.out.println(C==D);
+        System.out.println(A!=B);
+        System.out.println(A>B);
+        System.out.println(B<=D);
+    }
+}
---
+++ src/test/java/day03/Kosullu.java
@@ -0,0 +1,10 @@
+package day03;
+
+public class Kosullu {
+    public static void main(String[] args) {
+        int a=5, b=10, c;
+        c= (a==b)?2:3;
+        System.out.println(c);
+
+    }
+}
