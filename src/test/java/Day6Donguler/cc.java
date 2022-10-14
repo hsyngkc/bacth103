0c0db4665bf095d725bdb0d62f4fb6e44e484234
hsyngkc <hsyngkc14@gmail.com>
2022-10-14 16:01:02 +0300

    do while

--- src/test/java/Day6Donguler/cc.java
+++ src/test/java/Day6Donguler/cc.java
@@ -1,13 +1,37 @@
 package Day6Donguler;
-
+import java.util.Scanner;
 public class cc {
     public static void main(String[] args) {
-        for (int i = 1; i<= 10 ; i++){
-            System.out.println(i);
-        }
-        int i=1;
-        while (i<=10) {
-            System.out.println(i++);
-        }
+        Scanner not= new Scanner(System.in);
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        System.out.print("Matematik notunu giriniz:");
+        mat = not.nextInt();
+
+        System.out.print("Fizik notunu giriniz:");
+        fizik = not.nextInt();
+
+        System.out.print("Kimya notunu giriniz:");
+        kimya= not.nextInt();
+
+        System.out.print("Türkçe notonu girniz:");
+        turkce= not.nextInt();
+
+        System.out.print("Tarih notunu giriniz:");
+        tarih = not.nextInt();
+
+        System.out.print("Müzik  otunu giriniz:");
+        muzik = not.nextInt();
+
+        int toplam=(mat+ fizik+ kimya + tarih+ turkce+ muzik);
+
+        double sonuc= toplam/6.0;
+        System.out.println("Not ortalaması:"+ sonuc);
+
+        String str = sonuc > 60 ? "Sınıfı geçti" :  "Sınıfta kaldı";
+        System.out.println(str);
+
+
+
     }
 }
