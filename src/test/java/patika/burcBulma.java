fd6b14b1cd9cc17006c589cccc2ac255e8e45146
hsyngkc14 <hsyngkc14@gmail.com>
2022-10-26 21:28:10 +0300

    kasa

---
+++ src/test/java/patika/burcBulma.java
@@ -0,0 +1,21 @@
+package patika;
+
+import java.util.Scanner;
+
+public class burcBulma {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int day, month;
+
+        System.out.print("Doğduğunuz günü giriniz: ");
+        day  = input.nextInt();
+
+        System.out.print("Doğduğunuz ayı giriniz: ");
+        month = input.nextInt();
+
+       // çok uzun yazamadım.Mantığı kavradım ama.
+        }
+    }
+}
