06603ce63ef1119c55c1fc5e25f6f46aba951c97
hsyngkc14 <hsyngkc14@gmail.com>
2022-10-28 16:30:43 +0300

    new

---
+++ src/test/java/patika/basmakSayi.java
@@ -0,0 +1,20 @@
+package patika;
+
+import java.util.Scanner;
+
+public class basmakSayi {
+    public static void main(String[] args) {
+
+        int i, basNumber = 0, result = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        i = input.nextInt();
+
+        while (i != 0) {
+            basNumber = i % 10;
+            i /= 10;
+            result += basNumber;
+        }
+        System.out.println(result);
+    }
+}
