0898c6e8e7f120aaded726e7c59164123313aee1
hsyngkc14 <hsyngkc14@gmail.com>
2022-10-31 07:53:46 +0300

    new

---
+++ src/test/java/day7Method/palindrom.java
@@ -0,0 +1,23 @@
+package day7Method;
+
+import java.util.Scanner;
+
+public class palindrom {
+
+    static boolean isPalindrom(int number) {
+        int temp = number, reverseNumber = 0, lastnumber;
+        while (temp != 0) {
+            lastnumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastnumber;
+            temp /= 10;
+        }
+        if (number == reverseNumber)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println( isPalindrom(393));
+    }
+}
