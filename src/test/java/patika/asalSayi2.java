870fd69e8fa080665e22b0e15386d7013fd24c8c
hsyngkc14 <hsyngkc14@gmail.com>
2022-11-01 08:15:06 +0300

    new

---
+++ src/test/java/patika/asalSayi2.java
@@ -0,0 +1,21 @@
+package patika;
+import java.util.Scanner;
+public class asalSayi2 {
+    static boolean asal(int n, int j){
+
+        if(n == j)
+            return true;
+
+        if(n % j == 0)
+            return false;
+
+        return asal(n, j + 1);
+    }
+    public static void main(String[] args){
+        int x;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayıyı giriniz : ");
+        x = input.nextInt();
+        System.out.print(asal(x,2) ? x + " sayısı asaldır. !" : x + " sayısı asal değildir !" );
+    }
+}
